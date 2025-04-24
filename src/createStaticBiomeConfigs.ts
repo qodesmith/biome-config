e5d5ec8c9175ee82f12d33153a66a691478370b8
Qodesmith <theqodesmith@gmail.com>
2025-04-24 07:28:19 -0400

    Using Biome to format the generated Biome configs

--- src/createStaticBiomeConfigs.ts
+++ src/createStaticBiomeConfigs.ts
@@ -1,4 +1,14 @@
+import {Biome, Distribution} from '@biomejs/js-api'
+
 import {createBiomeConfig} from './createBiomeConfig'
+
+const biome = await Biome.create({distribution: Distribution.NODE})
+
+function formatJson(codeString: string): string {
+  return biome.formatContent(codeString, {
+    filePath: 'example.json',
+  }).content
+}
 
 const defaultConfig = createBiomeConfig({type: 'default'})
 const reactConfig = createBiomeConfig({type: 'react', includeJotaiHooks: false})
@@ -7,13 +17,16 @@ const reactExtraHooksConfig = createBiomeConfig({
   includeJotaiHooks: true,
 })
 
-/**
- * These two files are the static JSON files shipped with this package. They are
- * ready to be extended by the user.
- */
-await Bun.write('./dist/biome.json', JSON.stringify(defaultConfig, null, 2))
-await Bun.write('./dist/biomeReact.json', JSON.stringify(reactConfig, null, 2))
-await Bun.write(
-  './dist/biomeReactExtraHooks.json',
+const defaultConfigStr = formatJson(JSON.stringify(defaultConfig, null, 2))
+const reactConfigStr = formatJson(JSON.stringify(reactConfig, null, 2))
+const reactExtraHooksConfigStr = formatJson(
   JSON.stringify(reactExtraHooksConfig, null, 2)
 )
+
+/**
+ * These are the static JSON files shipped with this package. They are ready to
+ * be extended by the user.
+ */
+await Bun.write('./dist/biome.json', defaultConfigStr)
+await Bun.write('./dist/biomeReact.json', reactConfigStr)
+await Bun.write('./dist/biomeReactExtraHooks.json', reactExtraHooksConfigStr)
